8954a9c446243514a0c6a6bcfb95e64478be8270
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-11-21 16:32:15 +0100

    commit

---
+++ Eje09.java
@@ -0,0 +1,28 @@
+/*Ejercicio 9
+Realiza un programa que pida 8 números enteros y que luego muestre esos
+números junto con la palabra “par” o “impar” según proceda.
+*/
+
+public class Eje09 {
+    public static void main(String[] args) {
+
+        int[] array = new int[8];
+
+        for(int i=0; i<array.length; i++){
+            System.out.print("Introduzca su número: ");
+            array[i] = Integer.parseInt(System.console().readLine());
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]%2==0){
+                System.out.printf("%d: %d Par",(i+1),array[i]);
+            }else{
+                System.out.printf("%d: %d Impar",(i+1),array[i]);
+            }
+            System.out.println();
+        }
+
+    }
+}
